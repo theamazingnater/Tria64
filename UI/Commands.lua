25c0ed928effad9301055edac6dd2e745ff25eaa
Triarawn <55967100+theamazingnater@users.noreply.github.com>
2021-02-28 23:46:42 -0700

    Create Commands.lua

---
+++ UI/Commands.lua
@@ -0,0 +1,12 @@
+-- This is a modulescript!
+local module = {
+	printc = "print ";
+	help = "help";
+	info = "info";
+	erase = "erase";
+	sound = "play ";
+	loop = "loop";
+	stop = "stop";
+}
+
+return module
